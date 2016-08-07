80ac118a855df6dd60b8926d76e574653be7d792
chenhengjie <chenhengjie123@gmail.com>
2016-08-07 12:13:00 +0800

    [l14]: 移除固定失败的用例

--- scripts/l13_page_object/test_sum.py
+++ scripts/l13_page_object/test_sum.py
@@ -14,14 +14,14 @@ def test_check_sum_function(driver, logger):
     assert index_page.sum_result_label.text == str(first_arg + second_arg)
 
 
-def test_check_sum_function_should_fail(driver, logger):
-    first_arg = 1
-    second_arg = 2
-
-    # find elements
-    index_page = IndexPage(driver)
-    logger.info("输入两个加数 %d 和 %d, 并点击求和按钮" % (first_arg, second_arg))
-    index_page.caculate_sum(str(first_arg), str(second_arg))
-    logger.info("获取求和结果, 检查结果是否为 %d" % (first_arg + second_arg))
-    index_page.caculate_sum(str(first_arg), str(second_arg))
-    assert index_page.sum_result_label.text == str(first_arg + second_arg + 1)
+# def test_check_sum_function_should_fail(driver, logger):
+#     first_arg = 1
+#     second_arg = 2
+#
+#     # find elements
+#     index_page = IndexPage(driver)
+#     logger.info("输入两个加数 %d 和 %d, 并点击求和按钮" % (first_arg, second_arg))
+#     index_page.caculate_sum(str(first_arg), str(second_arg))
+#     logger.info("获取求和结果, 检查结果是否为 %d" % (first_arg + second_arg))
+#     index_page.caculate_sum(str(first_arg), str(second_arg))
+#     assert index_page.sum_result_label.text == str(first_arg + second_arg + 1)
